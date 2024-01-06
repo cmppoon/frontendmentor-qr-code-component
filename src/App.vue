9aaf8db893e3502b4e38e74ee162b1e56bc69d03
Chaiyapruek Muangsiri <cmp.poon@gmail.com>
2024-01-06 16:30:18 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import BaseCard from './components/BaseCard.vue'
+</script>
+
+<template>
+  <main class="h-full">
+    <BaseCard />
+  </main>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Outfit:wght@400;700&display=swap');
+
+html,
+body {
+  height: 100%;
+  font-family: 'Outfit', sans-serif;
+}
+
+#app {
+  height: 100%;
+  font-family: 'Outfit', sans-serif;
+}
+</style>
---
+++ src/components/BaseCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex flex-col justify-center items-center mx-auto w-60 sm:w-80 h-full">
+    <div class="p-4 flex flex-col bg-white border border-lightgray shadow-sm rounded-xl">
+      <img alt="QR Code" class="rounded-xl" src="../assets/image-qr-code.png" />
+
+      <div class="text-center p-4">
+        <p class="text-darkblue text-xl font-bold">
+          Improve your front-end skills by building projects
+        </p>
+        <p class="text-grayishblue text-sm mt-2">
+          Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
+        </p>
+      </div>
+    </div>
+
+    <div class="text-grayishblue text-center text-sm mt-4">
+      Challenge by
+      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+      Coded by <a href="https://github.com/cmppoon">Chaiyapruek Muangsiri</a>.
+    </div>
+  </div>
+</template>
